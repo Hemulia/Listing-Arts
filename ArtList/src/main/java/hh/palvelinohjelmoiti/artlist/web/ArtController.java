f0a384ec6969b3a1a639cbe2119b5c2109ac1cd6
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-04-13 02:02:05 +0300

    update 2

--- ArtList/src/main/java/hh/palvelinohjelmoiti/artlist/web/ArtController.java
+++ ArtList/src/main/java/hh/palvelinohjelmoiti/artlist/web/ArtController.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.palvelinohjelmoiti.artlist.domain.Art;
 import hh.palvelinohjelmoiti.artlist.domain.ArtRepository;
+import hh.palvelinohjelmoiti.artlist.domain.ArtistRepository;
 
 @Controller
 public class ArtController {
 
 	@Autowired
 	private ArtRepository repository;
+
+	@Autowired
+	private ArtistRepository arepository;
 
 	@RequestMapping(value = { "/", "/artlist" })
 	public String artList(Model model) {
@@ -37,10 +41,19 @@ public class ArtController {
 		return "redirect:../arts";
 	}
 
+	// Poisto
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	public String deleteBook(@PathVariable("id") Long artId, Model model) {
+	public String deleteArt(@PathVariable("id") Long artId, Model model) {
 		repository.deleteById(artId);
 		return "redirect:../artlist";
+	}
+
+	@RequestMapping(value = { "/add" })
+	public String addArt(Model model) {
+
+		model.addAttribute("art", new Art());
+		model.addAttribute("artist", arepository.findAll());
+		return "addart";
 	}
 
 }
