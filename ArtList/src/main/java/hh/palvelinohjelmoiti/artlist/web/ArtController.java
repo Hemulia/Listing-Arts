e2432851c409c1e18a81759f0a45b09edf9d97c2
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-04-13 01:17:35 +0300

    Delete

--- ArtList/src/main/java/hh/palvelinohjelmoiti/artlist/web/ArtController.java
+++ ArtList/src/main/java/hh/palvelinohjelmoiti/artlist/web/ArtController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,6 +35,12 @@ public class ArtController {
 	public String saveArt(@ModelAttribute Art art) {
 		repository.save(art);
 		return "redirect:../arts";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long artId, Model model) {
+		repository.deleteById(artId);
+		return "redirect:../artlist";
 	}
 
 }
